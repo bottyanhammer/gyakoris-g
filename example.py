59f158863b9f0c82b926897c0d133bd0589b5a53
bottyanhammer <bottyan.hammer@gmail.com>
2023-12-14 12:56:26 +0100

    Make numbers

--- example.py
+++ example.py
@@ -1,4 +1,38 @@
 import random
 
-print("Számok gyakorisága egy adott intervallumban")
-print("Mindjárt kicsongetnek, ki nem sz@ja le most ezt???")
+def isitPerfect():
+    pass
+
+def randomGenerator(s, e, a):
+    numbers = list()
+    for i in range(a):
+        numbers.append(random.randint(s, e))
+    return numbers
+
+def makeNumber(text):
+    #isCorrect = False
+    while True:    #not isCorrect:
+        n = input(text)
+        try:
+            n = int(n)
+            #isCorrect = True
+            return n
+        except ValueError:
+            print("Helytelen érték.")
+
+
+startMessage = "Kezdő érték: "
+endMessage = "Végérték: "
+amountMessage = "Értékek száma: "
+
+start = makeNumber(startMessage)
+end = makeNumber(endMessage)
+amount = makeNumber(amountMessage)
+
+randomGenerator(start, end, amount)
+
+
+    
+        
+
+
